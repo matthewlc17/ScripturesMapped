04802034c0163e5893c9fb551cca3a077d6e3329
Matthew Christensen <matt.christensen@byu.edu>
2018-01-30 15:10:33 -0700

    updated to video 6

--- javascript/scriptures.js
+++ javascript/scriptures.js
@@ -9,8 +9,9 @@
 /*global window */
 /*jslint browser: true */
 /*property
-    books, forEach, init, maxBookId, minBookId, onerror, onload, open, parse,
-    push, responseText, send, status
+    books, forEach, getElementById, hash, id, init, innerHTML, length,
+    maxBookId, minBookId, numChapters, onHashChanged, onerror, onload, open,
+    parse, push, responseText, send, split, status, substring
 */
 
 const scriptures = (function () {
@@ -30,11 +31,16 @@ const scriptures = (function () {
     let ajax;
     let cacheBooks;
     let init;
+    let onHashChanged;
+    let navigateHome;
+    let navigateBook;
+    let navigateChapter;
+    let bookChapterValid;
     /*==================================================================
      *              PRIVATE METHODS
      */
 
-    ajax = function(url, successCallback, failureCallback) {
+    ajax = function (url, successCallback, failureCallback) {
         let request = new XMLHttpRequest();
         request.open("GET", url, true);
 
@@ -55,7 +61,7 @@ const scriptures = (function () {
         request.send();
     };
 
-    cacheBooks = function(callback) {
+    cacheBooks = function (callback) {
         volumes.forEach(function (volume) {
             let volumeBooks = [];
             let bookId = volume.minBookId;
@@ -71,7 +77,7 @@ const scriptures = (function () {
         }
     };
 
-    init = function(callback) {
+    init = function (callback) {
         let booksLoaded = false;
         let volumesLoaded = false;
 
@@ -98,29 +104,82 @@ const scriptures = (function () {
             }
         );
     };
-    // function navigateHome() {
-    //     let html = "<div>The Old Testament</div><div>The New Testament</div>";
-    //     document.getElementByID("scriptures").innerHTM = html;
-    // }
+
+    onHashChanged = function () {
+        let ids = [];
+        let volumeId;
+        let bookId;
+        let chapter;
+        if (location.hash !== "" && location.hash.length > 1) {
+            //remove leading pound sign and split string on colon delimeters
+            ids = location.hash.substring(1).split(":");
+        }
+        if (ids.length <= 0) {
+            navigateHome();
+        } else if (ids.length === 1) {
+            //display single volume's table of contents
+            volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes[volumes.length - 1].id) {
+                navigateHome();
+            } else {
+                navigateHome(volumeId);
+            }
+        } else if (ids.length === 2) {
+            //display book's list of chapters
+            bookId = Number(ids[1]);
+            if (books[bookId] === "undefined") {
+                navigateHome();
+            } else {
+                navigateBook(bookId);
+            }
+        } else {
+            //display chapter contents
+            bookId = Number(ids[1]);
+            chapter = Number(ids[2]);
+
+            if (!bookChapterValid(bookId, chapter)) {
+                navigateHome();
+            } else {
+                navigateChapter(bookId, chapter);
+            }
+        }
+    };
+
+    navigateHome = function (volumeId) {
+        let html = "<div>The Book of Mormon</div><div>The Doctrine and Covenants</div><div>The Pearl of Great Price</div><div>The Old Testament</div><div>The New Testament</div><div>Selected Volume: " + volumeId + " </div>";
+        document.getElementById("scriptures").innerHTML = html;
+    };
+
+    navigateBook = function (bookId) {
+        document.getElementById("scriptures").innerHTML = "<div>Book ID: " + bookId + "</div>";
+    };
+
+    navigateChapter = function (bookId, chapter) {
+        document.getElementById("scriptures").innerHTML = "<div>Book ID: " + bookId + ", Chapter: " + chapter + "</div>";
+    };
+
+    bookChapterValid = function (bookId, chapter) {
+        let book = books[bookId];
+
+        if (book === "undefined" || chapter < 0 || chapter > book.numChapters) {
+            return false;
+        }
+        if (chapter === 0 && book.numChapters > 0) {
+            return false;
+        }
+        return true;
+    };
 
     /*==================================================
      *              PUBLIC API
      */
-    const api = {
+    return {
         init(callback) {
             init(callback);
+        },
+        onHashChanged() {
+            onHashChanged();
         }
-        // function onHashChanged() {
-        //     let ids = [];
-        //     if (location.hash !== "" && location.hash.length > 1) {
-        //         ids = location.hash.substring(1).split(':');
-        //     }
-        //     if (ids.length <= 0) {
-        //         navigateHome();
-        //     } else {
-        //
-        //     }
-        // }
     };
-    return api;
 }());
